418f79a497459d237f57814d4363b467a8a87486
Anil <anilmende2002@email.com>
2026-01-07 17:42:26 +0530

    Nto1 and 1toN

--- src/Nto1.java
+++ src/Nto1.java
@@ -1,7 +1,8 @@
 public class Nto1 {
     public static void main(String[] args) {
-        print(5);
+        //print(5);
         //printReverse(5);
+        printBoth(5);
     }
     static void print(int n){
         if(n == 0){
@@ -19,6 +20,15 @@ public class Nto1 {
             return;
         }
         printReverse(n-1);
+        System.out.println(n);
+    }
+
+    static void printBoth(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        printBoth(n-1);
         System.out.println(n);
     }
 }
